bb2371cf99bfe6b684a32a2c9af71a124c1a3f24
Dmitry Malchikov <dmalch@gmail.com>
2013-05-03 20:01:56 +0400

    Get payment

--- src/test/groovy/com/github/dmalch/PaymentTest.groovy
+++ src/test/groovy/com/github/dmalch/PaymentTest.groovy
@@ -2,13 +2,20 @@ package com.github.dmalch
 
 import org.junit.Test
 
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.is
+
 class PaymentTest {
     @Test
     public void testGenerateToken() throws Exception {
-        def generator = new TokenGenerator();
+        def generator = new CopyAndPay();
 
         def token = generator.generateToken()
 
         generator.executePayment(token)
+
+        def status = generator.getStatus(token)
+
+        assertThat status, is(true)
     }
 }
